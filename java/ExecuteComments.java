354af933b33f37005843e02e4f36116a855e2771
Krishnan <NK068049@cerner.net>
2020-09-15 18:17:42 +0530

    Adding java code for 2020 2^5

---
+++ java/ExecuteComments.java
@@ -0,0 +1,12 @@
+/******************************************************************************
+
+ Java Program  for cerner_2^5_2020
+
+ *******************************************************************************/
+public class ExecuteComments {
+    public static void main(String[] args)
+    {
+        // the line below this gives an output
+        // \u000d System.out.println("Stay safe Stay Healthy");
+    }
+}
